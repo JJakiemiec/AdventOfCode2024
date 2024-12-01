c8559fc8d235de37113cc701bef675dc88f9e036
Jacob Jakiemiec <jjakiemiec@gmail.com>
2024-12-01 00:55:32 -0900

    part 2 and refactor

--- day1.cpp
+++ day1.cpp
@@ -1,3 +1,7 @@
+//Jacob Jakiemiec
+//advent of code day 1
+//12-1-2024
+
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -13,9 +17,11 @@ using std::getline;
 using std::stringstream;
 #include <algorithm>
 using std::sort;
+#include <utility>
+using std::pair;
 
 
-void part1(){
+pair<vector<int>, vector<int>> parseList(){
     //initialize variables
     ifstream input("day1-1.txt");
     vector<int> list1;
@@ -27,8 +33,7 @@ void part1(){
     //parse input file into vectors
     while(getline(input, line)){
         stringstream s(line);
-        s >> first;
-        s >> second;
+        s >> first >> second;
         list1.push_back(stoi(first));
         list2.push_back(stoi(second));
     }    
@@ -37,7 +42,13 @@ void part1(){
     sort(list1.begin(), list1.end());
     sort(list2.begin(), list2.end());
     
-    //find the differences in each pair
+    return make_pair(list1, list2); 
+}
+
+//objective:
+// 1: find difference between sorted pairs
+// 2: add up total differences
+void part1(vector<int> list1, vector<int> list2){
     int total = 0;
     for(int i = 0; i < list1.size(); i++){
         int difference = list1[i] - list2[i];
@@ -48,8 +59,30 @@ void part1(){
     cout << total << endl;
 }
 
+
+//objective:
+// 1: find how many times a number in the first list appears in the second
+// 2: multiply frequency by the number in the first list
+// 3: add up totals
+void part2(vector<int> list1, vector<int> list2){
+    int total = 0;
+    for(int i: list1){
+        int count = 0;
+        for(int j: list2){
+            if(i == j){
+                count++;
+            }
+        }
+        total += i * count;
+    }
+    cout << total << endl;
+
+
+}
+
 int main(){
-    
-    part1();
+    pair<vector<int>, vector<int>> lists = parseList();
+    part1(lists.first, lists.second);
+    part2(lists.first, lists.second);
     return 0;
 }
