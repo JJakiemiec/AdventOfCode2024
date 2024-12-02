ed1d47797808648a2c1b5fa7dacb87a13a149fa1
Jacob Jakiemiec <jjakiemiec@gmail.com>
2024-12-01 20:07:56 -0900

    first commit day 2

---
+++ day2/day2.cpp
@@ -0,0 +1,29 @@
+//Jacob Jakiemiec
+//advent of code day 2
+//12-2-2024
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+using std::stoi;
+#include <fstream>
+using std::ifstream;
+using std::getline;
+#include <sstream>
+using std::stringstream;
+#include <algorithm>
+using std::sort;
+#include <utility>
+using std::pair;
+
+
+
+
+int main(){
+    cout << "hello" << endl;
+    return 0;
+}
