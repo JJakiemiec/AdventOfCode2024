8af3d5a80bfe0a5c054535b7cddf8a6235082bc4
Jacob Jakiemiec <jjakiemiec@gmail.com>
2024-12-01 21:22:34 -0900

    part 2 correct

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -65,6 +65,36 @@ int part1(vector<vector<int>> input){
     return count;
 }
 
+bool isRemovedSafe(vector<int> report){
+    bool isPermSafe = false;
+        for(int i=0;i<report.size();i++){
+            vector<int> perm;
+            for(int j = 0; j < report.size();j++){
+                if(i!=j){
+                    perm.push_back(report[j]);
+                }
+            }
+            for (int a: perm){
+                cout << a << ",";
+            }
+            cout << endl;
+            isPermSafe = isSafe(perm);
+            if (isPermSafe)
+                return true;
+        }
+    return false;
+}
+
+int part2(vector<vector<int>> input){
+    int count = 0;
+    for(vector<int> report: input){ 
+        if(isRemovedSafe(report)){
+            count++;
+        }
+    }
+    return count;
+}
+
 int main(){
     vector<vector<int>> list = parseFile("day2-1.txt");
     vector<int> test1 = {1,2,3,4};
@@ -75,9 +105,16 @@ int main(){
     vector<int> test6 = {4,3,1,2};
     vector<int> test7 = {1, 5, 10, 15};
     vector<int> test8 = {15, 10, 5, 1};
-    cout << true << endl;
-    cout << isSafe(test1) <<isSafe(test2) <<isSafe(test3) <<isSafe(test4) <<isSafe(test5) <<isSafe(test6) <<isSafe(test7) <<isSafe(test8) << endl;
-    cout << isSafe(test1)<<endl;
-    cout << part1(list);
+    vector<int> test9 = {10, 4, 1};
+    //cout << true << endl;
+    //cout << isSafe(test1) <<isSafe(test2) <<isSafe(test3) <<isSafe(test4) <<isSafe(test5) <<isSafe(test6) <<isSafe(test7) <<isSafe(test8) << endl;
+    //cout << isSafe(test1)<<endl;
+    //cout << part1(list) << endl;
+    //vector<vector<int>> t = {test1, test2, test3, test4, test5, test6, test7, test8};
+    //vector<vector<int>> t = {test9};
+    //cout << part2(t) << endl;
+    cout << isRemovedSafe(test9) << endl;
+    cout << part2(list) << endl;
+    cout << part1(list) << endl;
     return 0;
 }
