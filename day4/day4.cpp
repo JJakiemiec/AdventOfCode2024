c1a8866b9dac7d0cd43c492531506f6f9b554877
Jacob Jakiemiec <jjakiemiec@gmail.com>
2024-12-03 22:32:27 -0900

    last commit of the day

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -20,12 +20,66 @@ using std::regex_search;
 
 vector<vector<char>> parseInput(string file){
     vector<vector<char>> input;
+    ifstream f(file);
+    string line;
 
+    while(getline(f, line)){
+        vector<char> c(line.begin(), line.end());
+        input.push_back(c);
+    }
     return input;
 }
 
+void printInput(vector<vector<char>> input){
+    for(vector<char> i: input){
+        for(char j: i){
+            cout << j;
+        }
+        cout << endl;
+    }
+
+}
+
+int check(const vector<vector<char>> &input, int i, int j, int dx, int dy){
+    //TODO
+    //fix bounds issue
+    if(i > input.size() - (3 * dx) || j > input[0].size() - (3 * dy))
+        return 0;
+
+    if(input[i][j] == 'x')
+                if(input[i + dx][j + dy] == 'm')
+                    if(input[i + (2*dx)][j + (2*dy)] == 'a')
+                        if(input[i+ (3*dx)][j + (3*dy)] == 's'){
+                            cout << "YES" << endl;
+                            return 1;
+                        }
+    return 0;
+}
+
+int part1(vector<vector<char>> input){
+    int result = 0;
+
+    for(int i=0; i<input.size(); i++){
+        for(int j=0; j < input[0].size(); j++){
+            result += check(input, i, j, 0, 1);
+            result += check(input, i, j, 0, -1);
+            result += check(input, i, j, 1, 0);
+            result += check(input, i, j, -1, 0);
+            result += check(input, i, j, 1, 1);
+            result += check(input, i, j, 1, -1);
+            result += check(input, i, j, -1, 1);
+            result += check(input, i, j, -1, -1);
+        }
+    }
+
+    return result;
+}
+
 int main(){
-
-
+    //vector<vector<char>> test{{'x','m','a','s','x'}, {'1','2','x','m','a','s','3'},{'s','a','m','x'},{'x','s'},{'m','a'},{'a','m'},{'s','x'}};
+    vector<vector<char>> test = parseInput("day4test.txt");
+    vector<vector<char>> input = parseInput("day4.txt");
+    //printInput(input);
+    cout << part1(test) << endl;
     return 0;
 }
