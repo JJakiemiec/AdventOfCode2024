7493e69316a12054ac7a01686428ae18f588d94b
Jacob Jakiemiec <jjakiemiec@gmail.com>
2024-12-03 21:54:26 -0900

    first commit of day 4

---
+++ day4/day4.cpp
@@ -0,0 +1,31 @@
+//Jacob Jakiemiec
+//advent of code day 4
+//12-3-24
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+using std::stoi;
+#include <fstream>
+using std::ifstream;
+using std::getline;
+#include <regex>
+using std::regex;
+using std::smatch;
+using std::regex_search;
+
+vector<vector<char>> parseInput(string file){
+    vector<vector<char>> input;
+
+    return input;
+}
+
+int main(){
+
+
+    return 0;
+}
